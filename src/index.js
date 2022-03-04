5feec334e74d5543485c83c93318c0d12181b1bd
yw <yanbinwyb@163.com>
2022-03-04 14:19:39 +0800

    chore: remove ora

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 const path = require("path");
 const golb = require("glob");
 const chalk = require("chalk");
-const ora = require("ora");
-const spinner = ora({
-  color: 'green'
-})
 const { writeTempDirectory, zipDirectory } = require("./zipUtils");
 const UserHook = require("./hook");
 
@@ -21,22 +17,17 @@ class ZipFilePlugin {
         const options = this.options
         const basePath = options.path || compilation.outputOptions.path;
         console.log(chalk.green('Start finding files'));
-        spinner.start();
         const files = golb.sync(
           path.join(basePath, `./*.*`)
         );
         console.log(chalk.blue(`A total of ${chalk.red(files.length)} files were found`));
-        spinner.stop();
         console.log(chalk.green('Start compressing folder'));
-        spinner.start();
         const tempPath = await writeTempDirectory(files, options.name);
         const zipUrl = await zipDirectory({ ...options, tempPath, basePath: basePath });
         console.log(chalk.green('Compression complete'));
-        spinner.stop();
         zipHook.emitHook("zipDone", [zipUrl])
       } catch (err) {
         console.log(chalk.red(err));
-        spinner.stop();
       }
     });
   }
